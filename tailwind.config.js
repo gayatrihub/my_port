3d9e9510e4ffbfb1223eeb76a8cfbcc4b350d6b0
gayatrihub <161610508+gayatrihub@users.noreply.github.com>
2025-06-29 12:17:56 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
+// tailwind.config.js
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       animation: {
-      'spin-slow': 'spin 6s linear infinite',
-    },
+        'spin-slow': 'spin 6s linear infinite',
+        tilt: 'tilt 10s infinite linear',
+      },
+      keyframes: {
+        tilt: {
+          '0%': { transform: 'rotate(0deg)' },
+          '50%': { transform: 'rotate(1deg)' },
+          '100%': { transform: 'rotate(0deg)' },
+        },
+      },
     },
   },
   plugins: [],
-}
+};
