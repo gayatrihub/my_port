51cb1555c8c04dd04a1c7154574c99082e879bf2
gayatrihub <161610508+gayatrihub@users.noreply.github.com>
2025-06-29 12:56:11 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,20 @@ module.exports = {
     extend: {
       animation: {
         'spin-slow': 'spin 6s linear infinite',
-         'ping-slow': 'ping 6s cubic-bezier(0, 0, 0.2, 1) infinite',
+        'ping-slow': 'ping 6s cubic-bezier(0, 0, 0.2, 1) infinite',
         tilt: 'tilt 10s infinite linear',
+        float: 'float 5s ease-in-out infinite',
+        pingLong: 'ping 6s cubic-bezier(0, 0, 0.2, 1) infinite',
       },
       keyframes: {
-        tilt: {
+         tilt: {
           '0%': { transform: 'rotate(0deg)' },
           '50%': { transform: 'rotate(1deg)' },
           '100%': { transform: 'rotate(0deg)' },
+        },
+        float: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-10px)' },
         },
       },
     },
