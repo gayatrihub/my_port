f1dd7b5a5ef81249cfa30f2673661d007a33834b
gayatrihub <161610508+gayatrihub@users.noreply.github.com>
2025-08-02 00:53:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ import {
 } from "react-icons/fa";
 import { SiCplusplus } from "react-icons/si";
 import { FaUsers, FaLightbulb } from "react-icons/fa6";
+import EducationTimeline from "./components/EducationTimeline";
 
 const androidProjects = [
 {
